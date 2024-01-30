c1f2f309b9ec63e721e6590318d7858ee3515f47
emes-g <saltishDev@gmail.com>
2024-01-31 03:08:03 +0900

    docs : change the comment for links' setColor()
    주석 내용과 jquery 코드가 동치가 아니어서 주석 내용을 수정했다.

--- colors.js
+++ colors.js
@@ -12,10 +12,9 @@ var links = {
     setColor: function (color) {
         // var links = document.querySelectorAll('a');
 
-        // for (var i = 0; i < links.length; i++) {
-        //     if (window.getComputedStyle(links[i]).color != 'rgb(255, 0, 0)')
-        //         links[i].style.color = color;
-        // }
+        // for (var i = 0; i < links.length; i++)
+        //     links[i].style.color = color;
+        // document.getElementById('active').style.color = 'red';
         $('a').css('color', color);
         $('#active').css('color', 'red');
     }
