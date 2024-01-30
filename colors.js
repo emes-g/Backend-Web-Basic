f0ab9633e9861d65715f97fcb66e16bc4454cc39
emes-g <saltishDev@gmail.com>
2024-01-30 22:50:17 +0900

    refactor : refactor colors.js by using jquery library

--- colors.js
+++ colors.js
@@ -1,19 +1,23 @@
 var body = {
     setColor: function (color) {
-        document.querySelector('body').style.color = color;
+        // document.querySelector('body').style.color = color;
+        $('body').css('color', color);
     },
     setBackgroundColor: function (color) {
-        document.querySelector('body').style.backgroundColor = color;
+        // document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color);
     }
 }
 var links = {
     setColor: function (color) {
-        var links = document.querySelectorAll('a');
+        // var links = document.querySelectorAll('a');
 
-        for (var i = 0; i < links.length; i++) {
-            if (window.getComputedStyle(links[i]).color != 'rgb(255, 0, 0)')
-                links[i].style.color = color;
-        }
+        // for (var i = 0; i < links.length; i++) {
+        //     if (window.getComputedStyle(links[i]).color != 'rgb(255, 0, 0)')
+        //         links[i].style.color = color;
+        // }
+        $('a').css('color', color);
+        $('#active').css('color', 'red');
     }
 }
 function dayNightHandler(self) {
@@ -21,7 +25,7 @@ function dayNightHandler(self) {
         self.value = 'day';
         body.setBackgroundColor('black');
         body.setColor('white');
-        links.setColor('white');
+        links.setColor('yellow');
     }
     else {
         self.value = 'night';
