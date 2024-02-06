13f5dc142f333ab595d342091c2149cd28ee0a62
emes-g <saltishDev@gmail.com>
2024-02-06 19:25:39 +0900

    refactor : change main.js by using the function concept
    함수 개념을 이용하여 main.js에서 중복되는 부분을 한 번만 작성

--- nodejs/main.js
+++ nodejs/main.js
@@ -3,13 +3,40 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, body) {
+    return `
+    <!doctype html>
+    <html>
+
+    <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+        <script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    </head>
+    <body>
+        <h1><a href="/">WEB</a></h1>
+        ${list}
+        ${body}
+    </body>
+
+    </html>
+    `;
+}
+
+function templateList(filelist) {
+    var list = '<ul>';
+    for (var i = 0; i < filelist.length; i++)
+        list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+    list += '</ul>';
+    return list;
+}
+
 // 2. http 모듈로 서버를 생성, 사용자로부터 http 요청이 들어오면 function 블럭 내부의 코드를 실행해서 응답 
 var app = http.createServer(function (request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
 
-    console.log(url.parse(_url, true));
     if (pathname === '/') {
         // 접속한 페이지가 메인 홈페이지인 경우
         if (queryData.id === undefined) {
@@ -20,74 +47,26 @@ var app = http.createServer(function (request, response) {
             fs.readdir('./data', function (error, filelist) {
                 var title = 'Welcome';
                 var description = 'Hello, Node.js';
-                var list = '<ul>';
-                for (var i = 0; i < filelist.length; i++)
-                    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-                list += '</ul>';
-                var template = `
-                <!doctype html>
-                <html>
-
-                <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                    <script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-                </head>
-                <body>
-                    <h1><a href="/">WEB</a></h1>
-                    <div id="grid">
-                        ${list}
-                        <div id="article">
-                            <h2>${title}</h2>
-                            ${description}
-                        </div>
-                    </div>
-                </body>
-
-                </html>
-                `;
+                var list = templateList(filelist);
+                var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
                 response.writeHead(200);    // 200 : 파일을 성공적으로 전달함
                 response.end(template);
             })
 
-        // 메인 홈페이지가 아닌 경우
+            // 메인 홈페이지가 아닌 경우
         } else {
             fs.readdir('./data', function (error, filelist) {
-                var list = '<ul>';
-                for (var i = 0; i < filelist.length; i++)
-                    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-                list += '</ul>';
                 fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
                     var title = queryData.id;
-                    var template = `
-                    <!doctype html>
-                    <html>
-
-                    <head>
-                        <title>WEB1 - ${title}</title>
-                        <meta charset="utf-8">
-                        <script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-                    </head>
-                    <body>
-                        <h1><a href="/">WEB</a></h1>
-                        <div id="grid">
-                            ${list}
-                            <div id="article">
-                                <h2>${title}</h2>
-                                ${description}
-                            </div>
-                        </div>
-                    </body>
-
-                    </html>
-                    `;
+                    var list = templateList(filelist);
+                    var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
                     response.writeHead(200);
                     response.end(template);
                 });
             });
         }
 
-    // path가 '/'이 아닌 경우
+        // path가 '/'이 아닌 경우
     } else {
         response.writeHead(404);    // 404 : 파일을 찾을 수 없음
         response.end('Not found');
