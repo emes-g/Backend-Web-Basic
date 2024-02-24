dd630a956f99ce61c524e5bb59376d1f89d86f36
emes-g <saltishDev@gmail.com>
2024-02-24 20:10:25 +0900

    refactor : apply my middle-ware
    현재 경로의 data 디렉토리에 존재하는 파일을 읽어오는 코드를
    middle-ware로 구현

--- express/main.js
+++ express/main.js
@@ -11,69 +11,73 @@ var bodyParser = require('body-parser')
 var compression = require('compression')
 
 // middle-ware
+// 1. third-party
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(compression())  // compress all responses
 
-// route, routing
-// 접속한 페이지가 메인 홈페이지인 경우
-app.get('/', function (request, response) {
+// 2. my middle-ware
+app.get('*', function (request, response, next) {
     /*
       첫번째 인자로 파일 목록을 읽을 폴더를 가져오고,
       콜백함수의 두번째 인자로 폴더의 파일목록을 가져옴
     */
     fs.readdir('./data', function (error, filelist) {
-        var title = 'Welcome';
-        var description = 'Hello, Node.js';
-        var list = template.list(filelist);
-        var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`);
-        response.send(html);
+        request.list = filelist;
+        next();
     })
+})
+
+// route, routing
+// 접속한 페이지가 메인 홈페이지인 경우
+app.get('/', function (request, response) {
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
+    var list = template.list(request.list);
+    var html = template.HTML(title, list,
+        `<h2>${title}</h2>${description}`,
+        `<a href="/create">create</a>`);
+    response.send(html);
 })
 
 // 메인 홈페이지가 아닌 경우
 app.get('/page/:pageId', function (request, response) {
-    fs.readdir('./data', function (error, filelist) {
-        var filteredId = path.parse(`${request.params.pageId}`).base;
+    var filteredId = path.parse(`${request.params.pageId}`).base;
 
-        fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-            var title = filteredId;
-            var sanitizedTitle = sanitizeHtml(title);
-            var sanitizedDescription = sanitizeHtml(description);
-            var list = template.list(filelist);
-            var html = template.HTML(sanitizedTitle, list,
-                `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-                `<a href="/create">create</a>
+    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+        var title = filteredId;
+        var sanitizedTitle = sanitizeHtml(title);
+        var sanitizedDescription = sanitizeHtml(description);
+        var list = template.list(request.list);
+        var html = template.HTML(sanitizedTitle, list,
+            `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+            `<a href="/create">create</a>
                  <a href="/update/${sanitizedTitle}">update</a>
                  <form action="/delete_process" method="post">
                     <input type="hidden" name="id" value="${sanitizedTitle}">
                     <input type="submit" value="delete">
                  </form>
                 `);
-            response.send(html);
-        })
+        response.send(html);
     })
 })
 
 // create 버튼을 클릭한 경우
 app.get('/create', function (request, response) {
-    fs.readdir('./data', function (error, filelist) {
-        var title = 'WEB - create';
-        var list = template.list(filelist);
-        var html = template.HTML(title, list, `
+    var title = 'WEB - create';
+    var list = template.list(request.list);
+    var html = template.HTML(title, list, `
         <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p><textarea name="description" placeholder="description" cols=120 rows=5></textarea></p>
             <p><input type="submit"></p>
         </form>
         `, '');
-        response.send(html);
-    })
+    response.send(html);
 })
 
 // /create 페이지에서 제출 버튼을 클릭한 경우
 app.post('/create_process', function (request, response) {
+    console.log(request.list);
     var post = request.body;
     var title = post.title;
     var description = post.description;
@@ -86,11 +90,11 @@ app.post('/create_process', function (request, response) {
 // update 버튼을 클릭한 경우
 app.get('/update/:pageId', function (request, response) {
     var filteredId = path.parse(`${request.params.pageId}`).base;
-    fs.readdir('./data', function (error, filelist) {
-        fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-            var title = filteredId;
-            var list = template.list(filelist);
-            var html = template.HTML(title, list, `
+
+    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+        var title = filteredId;
+        var list = template.list(request.list);
+        var html = template.HTML(title, list, `
                 <form action="/update_process" method="post">
                     <p><input type="hidden" name="id" value="${title}"></p>
                     <p><input type="text" name="title" placeholder="title" value="${title}"></p>
@@ -98,8 +102,7 @@ app.get('/update/:pageId', function (request, response) {
                     <p><input type="submit"></p>
                 </form>
                 `, '');
-            response.send(html);
-        });
+        response.send(html);
     });
 })
 
