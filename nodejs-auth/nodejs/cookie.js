cea9882def5c1a53c695ea4cf90861eb9e7b0a1f
emes-g <saltishDev@gmail.com>
2024-02-27 00:18:45 +0900

    feat : add permanent cookie
    세션 쿠키 외에 permanent 쿠키도 만들어 봄

--- nodejs-auth/nodejs/cookie.js
+++ nodejs-auth/nodejs/cookie.js
@@ -17,7 +17,13 @@ http.createServer(function (request, response) {
         setHeader()는 writeHeader()로 합쳐질 수 있다.
     */
     response.writeHead(200, {
-        'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
+        'Set-Cookie': [
+            // session : 웹브라우저를 끄면 사라지는 휘발성 쿠키
+            'yummy_cookie=choco', 
+            'tasty_cookie=strawberry',
+            // permanent : 지정된 기간동안 살아있는 쿠키
+            `Permanent=cookies; Max-age=${60*60*24*30}`
+        ]
     })
     response.end('Cookie!!');
 }).listen(3000)
